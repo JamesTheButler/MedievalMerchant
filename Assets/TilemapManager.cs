cb9389c8341e5b1e2dae351037a8c4a052139cf6
JamesTheButler <ille.tom@outlook.com>
2025-08-21 11:32:48 +0200

    add and assign pixel art assets

--- Assets/TilemapManager.cs
+++ Assets/TilemapManager.cs
@@ -15,11 +15,13 @@ public class TilemapManager : MonoBehaviour
 
     public void PlaceTiles(int size, List<Vector2Int> townLocations)
     {
+        var origin = new Vector3Int(-(size / 2), -(size / 2), 0);
+        
         for (var x = 0; x < size; x++)
         {
             for (var y = 0; y < size; y++)
             {
-                var pos = new Vector3Int(x, y, 0);
+                var pos = origin + new Vector3Int(x, y, 0);
                 tilemap.SetTile(pos, grassTile);
             }
         }
