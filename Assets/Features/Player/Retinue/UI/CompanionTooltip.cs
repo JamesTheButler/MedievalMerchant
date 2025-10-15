3f11c716df1ab8ffe3c5d643e939c77864e465af
JamesTheButler <ille.tom@outlook.com>
2025-10-15 10:30:13 +0200

    companion tooltip stub

---
+++ Assets/Features/Player/Retinue/UI/CompanionTooltip.cs
@@ -0,0 +1,13 @@
+using Common.UI;
+using Features.Player.Retinue.Config;
+
+namespace Features.Player.Retinue.UI
+{
+    public sealed class CompanionTooltip : TooltipBase<CompanionConfigData>
+    {
+        public override void SetData(CompanionConfigData data)
+        {
+            
+        }
+    }
+}
---
+++ Assets/Features/Player/Retinue/UI/CompanionTooltipHandler.cs
@@ -0,0 +1,7 @@
+using Common.UI;
+using Features.Player.Retinue.Config;
+
+namespace Features.Player.Retinue.UI
+{
+    public sealed class CompanionTooltipHandler : TooltipHandlerBase<CompanionConfigData> { }
+}
