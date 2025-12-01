c27adb8c48a19a981dd6f345460c6d9353d6f582
JamesTheButler <ille.tom@outlook.com>
2025-12-01 11:38:50 +0100

    track progression on level complete

---
+++ Assets/Features/Levels/Serialization/ProgressionUpdater.cs
@@ -0,0 +1,24 @@
+using Common;
+using UnityEngine;
+
+namespace Features.Levels.Serialization
+{
+    public sealed class ProgressionUpdater : MonoBehaviour
+    {
+        private Model _model;
+        
+        private void Awake()
+        {
+            _model = Model.Instance;
+        }
+
+        public void LevelCompleted()
+        {
+            var completionDate = Model.Instance.Date;
+            var levelSaveData = new CompletedLevelSaveData(completionDate);
+            var levelIndex = _model.LevelInfo.InternalIndex;
+            
+            ProgressionModel.Instance.UpdateCompletedLevel(levelIndex, levelSaveData);
+        }
+    }
+}
