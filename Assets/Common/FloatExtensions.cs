6e447dbc516efaed3527cdfc5b0c6f8b88604cb1
JamesTheButler <ille.tom@outlook.com>
2025-12-12 16:02:03 +0100

    remove long fractions in percent strings

--- Assets/Common/FloatExtensions.cs
+++ Assets/Common/FloatExtensions.cs
@@ -24,7 +24,7 @@ namespace Common
         public static string ToPercentString(this float f, bool forceSign = false)
         {
             var sign = forceSign ? f.Sign() : string.Empty;
-            return $"{sign}{f * 100}%";
+            return $"{sign}{f * 100:0.##}%";
         }
     }
 }
