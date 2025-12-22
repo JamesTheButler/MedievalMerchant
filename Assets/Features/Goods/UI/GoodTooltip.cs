12cfa9ede8b16919d26324e5c11d170bc5a0b040
JamesTheButler <ille.tom@outlook.com>
2025-12-22 15:41:46 +0100

    show current price in good tooltip, when applicaple

--- Assets/Features/Goods/UI/GoodTooltip.cs
+++ Assets/Features/Goods/UI/GoodTooltip.cs
@@ -3,6 +3,7 @@ using Common.Infrastructure;
 using Common.Types;
 using Common.UI.Tooltips;
 using Features.Goods.Config;
+using Features.Towns;
 using NaughtyAttributes;
 using TMPro;
 using UnityEngine;
@@ -13,10 +14,7 @@ namespace Features.Goods.UI
     public class GoodTooltip : TooltipBase<Good>
     {
         [SerializeField, Required]
-        private TMP_Text nameText, priceText;
-        
-        [SerializeField, Required] 
-        private GameObject currentPriceLine;
+        private TMP_Text nameText, priceText, currentPriceText;
 
         [SerializeField, Required]
         private Image goodImage, tierImage, regionImage;
@@ -25,7 +23,10 @@ namespace Features.Goods.UI
         private GoodsConfig _goodsConfig;
         private TierResources _tierIcons;
         private RegionResources _region;
+        private Selection _selection;
 
+        private Good _good;
+        private Town _selectedTown;
         protected override void Awake()
         {
             base.Awake();
@@ -33,11 +34,15 @@ namespace Features.Goods.UI
             _goodsResources = ResourceManager.Instance.GoodsResources;
             _tierIcons = ResourceManager.Instance.TierResources;
             _region = ResourceManager.Instance.RegionResources;
+
+            _selection = GameplayContext.Instance.Selection;
         }
 
         protected override void UpdateUI(Good data)
         {
-            var goodData = _goodsResources.ConfigData[data];
+            _good = data;
+            
+            var goodData = _goodsResources.ConfigData[_good];
             var tier = goodData.Tier;
             var price = _goodsConfig.BasePriceData[tier];
             var tierIcon = _tierIcons.Icons[tier];
@@ -51,9 +56,39 @@ namespace Features.Goods.UI
             tierImage.sprite = tierIcon;
             regionImage.sprite = regionIcon.Icon;
 
-            currentPriceLine.SetActive(false);
+            _selection.SelectedTown.Observe(OnTownChanged);
         }
 
-        public override void Reset() { }
+        private void OnTownChanged(Town newTown)
+        {
+            // unobserve the old price
+            _selectedTown?.PriceManager.GetPrice(_good)?.StopObserving(OnTownPriceChanged);
+            
+            _selectedTown = newTown;
+            if (newTown == null)
+            {
+                currentPriceText.text = "-";
+            }
+            else
+            {
+                var priceInTown = newTown.PriceManager.GetPrice(_good);
+                priceInTown?.Observe(OnTownPriceChanged);
+                if (priceInTown == null)
+                {
+                    currentPriceText.text = "-";
+                }
+            }
+        }
+
+        private void OnTownPriceChanged(float price)
+        {
+            currentPriceText.text = $"{price:0.#}";
+        }
+
+        public override void Reset()
+        {
+            _selection.SelectedTown.StopObserving(OnTownChanged);
+            _selection.SelectedTown.Value?.PriceManager?.GetPrice(_good)?.StopObserving(OnTownPriceChanged);
+        }
     }
 }
