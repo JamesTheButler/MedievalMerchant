95d4783e265b9fe1916118e819f9a10d88f73558
JamesTheButler <ille.tom@outlook.com>
2025-12-30 15:29:17 +0100

    add notificatoin debugger

---
+++ Assets/Features/Notifications/UI/NotifDebugger.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using Common.Infrastructure;
+using Features.Notifications.Logic;
+using NaughtyAttributes;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Features.Notifications.UI
+{
+    public sealed class NotifDebugger : MonoBehaviour
+    {
+        [SerializeField, Required]
+        private Image backgroundImage;
+
+        [SerializeField, Required]
+        private TMP_Text text;
+
+        private readonly List<Color> _colors = new()
+        {
+            Color.red,
+            Color.green,
+            Color.blue,
+        };
+
+        private int _runningIndex;
+
+        private void Start()
+        {
+            GameplayContext.Instance.Services.NotificationService.NotificationPosted += OnNotifPosted;
+        }
+
+        private void OnNotifPosted(Notification notification)
+        {
+            text.text = notification.Title;
+            backgroundImage.color = _colors[_runningIndex++ % _colors.Count];
+        }
+    }
+}
