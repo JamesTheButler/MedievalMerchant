18cfd6978cad34263e4e43267ac9869e8714889a
JamesTheButler <ille.tom@outlook.com>
2026-01-04 12:19:54 +0100

    more tutorial topics and buttons

--- Assets/Features/Tutorial/TutorialTopic.cs
+++ Assets/Features/Tutorial/TutorialTopic.cs
@@ -1,18 +1,13 @@
-﻿using System;
-
-namespace Features.Tutorial
+﻿namespace Features.Tutorial
 {
     public enum TutorialTopic
     {
         Intro = 0,
         Controls = 1,
         Retinue = 2,
-        [Obsolete]
         Caravan = 3,
         Development = 4,
         Town = 5,
-        [Obsolete]
-        Reputation = 6,
         Production = 7
     }
 }
