afd55c1b654cd8031a8864d3b68fbd7213a39f9a
JamesTheButler <ille.tom@outlook.com>
2026-01-05 16:47:45 +0100

    improve gamespeed controls

---
+++ Assets/Features/Ticking/UI/GamePausedIndicator.cs
@@ -0,0 +1,27 @@
+﻿using Common.Infrastructure;
+using Features.Ticking.Logic;
+using UnityEngine;
+
+namespace Features.Ticking.UI
+{
+    public sealed class GamePausedIndicator : MonoBehaviour
+    {
+        private GameSpeedModel _gameSpeedModel;
+
+        private void Awake()
+        {
+            _gameSpeedModel = GameplayContext.Instance.Model.GameSpeed;
+            _gameSpeedModel.IsPaused.Observe(OnPausedChanged);
+        }
+
+        private void OnDestroy()
+        {
+            _gameSpeedModel.IsPaused.StopObserving(OnPausedChanged);
+        }
+
+        private void OnPausedChanged(bool isPaused)
+        {
+            gameObject.SetActive(isPaused);
+        }
+    }
+}
