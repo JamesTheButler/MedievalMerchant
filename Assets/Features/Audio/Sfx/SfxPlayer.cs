c8513062e0aee5bf6fbd1728e53bfad06433b935
JamesTheButler <ille.tom@outlook.com>
2026-01-15 21:29:37 +0100

    stop duplicate parallel sfx

--- Assets/Features/Audio/Sfx/SfxPlayer.cs
+++ Assets/Features/Audio/Sfx/SfxPlayer.cs
@@ -63,6 +63,7 @@ namespace Features.Audio.Sfx
                 return;
             }
 
+            uiAudioSource.Stop();
             uiAudioSource.PlayOneShot(audioClip);
         }
 
@@ -77,6 +78,7 @@ namespace Features.Audio.Sfx
                 return;
             }
 
+            gameAudioSource.Stop();
             gameAudioSource.PlayOneShot(audioClip);
         }
     }
