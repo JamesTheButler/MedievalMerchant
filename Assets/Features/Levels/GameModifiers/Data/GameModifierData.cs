9819886606e6ceb58ef5125cde64d81bb3dda906
JamesTheButler <ille.tom@outlook.com>
2026-02-20 17:31:02 +0100

    localize level modifiers

--- Assets/Features/Levels/GameModifiers/Data/GameModifierData.cs
+++ Assets/Features/Levels/GameModifiers/Data/GameModifierData.cs
@@ -2,20 +2,21 @@
 using Common.Utility;
 using Features.Levels.GameModifiers.Effects.Data;
 using UnityEngine;
+using UnityEngine.Localization;
 
 namespace Features.Levels.GameModifiers.Data
 {
     public abstract class GameModifierData : ScriptableObject
     {
-        [field: SerializeField]
-        public string Title { get; private set; }
+        [SerializeField]
+        private LocalizedString title, description;
 
-        [field: SerializeField, TextArea]
-        public string Description { get; private set; }
+        public string Title => title.GetLocalizedString();
+        public string Description => description.GetLocalizedString();
 
         [field: SerializeReference, SubclassSelector]
         public List<EffectData> Effects { get; private set; }
-        
+
         public string EffectsString => Effects.AggregateString(effect => $"- {effect.Description}\n");
     }
 }
