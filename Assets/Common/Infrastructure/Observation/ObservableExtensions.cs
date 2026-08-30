c1115d5c03e4f731d0421ca891cd222bc87a1e0d
JamesTheButler <ille.tom@outlook.com>
2026-08-30 17:03:18 +0200

    add ObservableExtensions.Combine

--- Assets/Common/Infrastructure/Observation/ObservableExtensions.cs
+++ Assets/Common/Infrastructure/Observation/ObservableExtensions.cs
@@ -22,5 +22,20 @@ namespace Common.Infrastructure.Observation
         {
             return observable.Transform(value => -value);
         }
+
+        public static DerivedObservable<TOut> Combine<TIn1, TIn2, TOut>(
+            ReadOnlyObservable<TIn1> in1,
+            ReadOnlyObservable<TIn2> in2,
+            Func<TIn1, TIn2, TOut> combine)
+        {
+            var result = new DerivedObservable<TOut>(combine.Invoke(in1.Value, in2.Value));
+
+            result.TrackSource(
+                in1.Observe(value1 => result.Value = combine.Invoke(value1, in2.Value), false),
+                in2.Observe(value2 => result.Value = combine.Invoke(in1.Value, value2), false)
+            );
+
+            return result;
+        }
     }
 }
